206d55d14a2256421d0f519f1b7c63dbad12cae4
Panos <panosvlchs@gmail.com>
2014-11-21 00:46:01 +0200

    modifying ListDoublePoints: Link&test data

--- ListDoublePoints.java
+++ ListDoublePoints.java
@@ -1,7 +1,22 @@
 public class ListDoublePoints {
+
+	public Link {
+		public Object data;             // data item (Object)
+		public Link next;
+	}
+	
+		public Link(Object o) { // constructor
+			data = o;
+		}
+	
+	public void displayLink() {
+	
+		System.out.print("{"+data.toString()+"}");
+	}  
 	
 	// contains DoublePoints
-	DoublePoint a = new DoublePoint(2,1);
-
-	DoublePoint c = new DoublePoint()
+	DoublePoint a = new DoublePoint(1, 9);
+	DoublePoint b = new DoublePoint(2,10);
+	DoublePoint c = new DoublePoint(4,8);
+	DoublePoint d = new DoublePoint(9,2);
 }
