641403c9b539db2a9dd54708e22f41d4c3031d1c
Panagiotis Vlachos <panosvlchs@gmail.com>
2014-12-01 02:20:25 +0200

    coding skyline algorithm 1

--- SkylineAlg1.java
+++ SkylineAlg1.java
@@ -1,27 +1,52 @@
 public class SkylineAlg1 {
 
 		DoublePoint  previous = first; // αρχή της λίστας
-		DoublePoint current = previous.next; // το δεύτερο στοιχείο της λίστας
+		DoublePoint  current = previous.next; // το δεύτερο στοιχείο της λίστας
 
-		// foreach p in list |p|
-		while (current != null) 
+		// για κάθε σημείο p 
+		while (previous != null) 
 		{
 			
-			// foreach q in list |q|
-			while (next != null)  
+			// για κάθε σημείο q 
+			while (current != null)  
 			{
-				//    if p dominates q
-				if (current.x + current.y <= next.x + next.y)
+				// έλεγχος Χ
+				if (previous.x <= current.x)
 				{
-					// delete q
-				}
-				else
-				{
-					// delete previous
-					// next p
+					if (previous.x == current.x)
+					{
+						if (previous.y < current.y)
+							// ΚΥΡΙΑΡΧΙΑ p στο q
+							delete(current.x, current.y);
+						else
+						{
+							// ΚΥΡΙΑΡΧΙΑ q στο p
+							delete(previous.x, previous.y);
+							break;
+						}
+					}
 				}
 
+				// έλεγχος y
+				if (previous.y <= current.y)
+				{
+					if (previous.y == current.y)
+					{
+						if (previous.x < current.x)
+							// ΚΥΡΙΑΡΧΙΑ p στο q
+							delete(current.x, current.y);
+						else
+						{
+							// ΚΥΡΙΑΡΧΙΑ q στο p
+							delete(previous.x, previous.y);
+							break;
+						}
+					}
+				}
+				current = current.next;
+				
 			}
+			previous = previous.next;
 		}
 	
 }
