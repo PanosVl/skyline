bbefcd9514976f0379988016e9f846ce36ff14b5
Panagiotis Vlachos <panosvlchs@gmail.com>
2014-12-02 01:27:42 +0200

    I shouldn't code when I need sleep

--- SkylineAlg1.java
+++ SkylineAlg1.java
@@ -1,52 +1,54 @@
 public class SkylineAlg1 {
 
-		DoublePoint  previous = first; // αρχή της λίστας
-		DoublePoint  current = previous.next; // το δεύτερο στοιχείο της λίστας
+		public ListDoublePoints skyline1( ListDoublePoints list ) {
+			DoublePoint  previous = first; // αρχή της λίστας
+			DoublePoint  current = previous.next; // το δεύτερο στοιχείο της λίστας
 
-		// για κάθε σημείο p 
-		while (previous != null) 
-		{
-			
-			// για κάθε σημείο q 
-			while (current != null)  
+			// για κάθε σημείο p 
+			while (previous != null) 
 			{
-				// έλεγχος Χ
-				if (previous.x <= current.x)
-				{
-					if (previous.x == current.x)
-					{
-						if (previous.y < current.y)
-							// ΚΥΡΙΑΡΧΙΑ p στο q
-							delete(current.x, current.y);
-						else
-						{
-							// ΚΥΡΙΑΡΧΙΑ q στο p
-							delete(previous.x, previous.y);
-							break;
-						}
-					}
-				}
-
-				// έλεγχος y
-				if (previous.y <= current.y)
-				{
-					if (previous.y == current.y)
-					{
-						if (previous.x < current.x)
-							// ΚΥΡΙΑΡΧΙΑ p στο q
-							delete(current.x, current.y);
-						else
-						{
-							// ΚΥΡΙΑΡΧΙΑ q στο p
-							delete(previous.x, previous.y);
-							break;
-						}
-					}
-				}
-				current = current.next;
 				
-			}
-			previous = previous.next;
+				// για κάθε σημείο q 
+				while (current != null)  
+				{
+					// έλεγχος Χ
+					if (previous.x <= current.x)
+					{
+						if (previous.x == current.x)
+						{
+							if (previous.y < current.y)
+								// ΚΥΡΙΑΡΧΙΑ p στο q
+								delete(current.x, current.y);
+							else
+							{
+								// ΚΥΡΙΑΡΧΙΑ q στο p
+								delete(previous.x, previous.y);
+								break;
+							}
+						}
+					}
+
+					// έλεγχος y
+					if (previous.y <= current.y)
+					{
+						if (previous.y == current.y)
+						{
+							if (previous.x < current.x)
+								// ΚΥΡΙΑΡΧΙΑ p στο q
+								delete(current.x, current.y);
+							else
+							{
+								// ΚΥΡΙΑΡΧΙΑ q στο p
+								delete(previous.x, previous.y);
+								break;
+							}
+						}
+					}
+					current = current.next;
+					
+				}
+				previous = previous.next;
+			}	
 		}
 	
 }
